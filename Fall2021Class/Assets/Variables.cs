9d6ab2efec5ef119b7fcb7ed5d70ab313c039525
Caleb Terrasas <caleb.terrasas@uvu.edu>
2021-10-02 00:54:03 -0600

    Created Variables, practiced int and string.

---
+++ Fall2021Class/Assets/Variables.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int myNum = 50;
+        string myName = "Caleb"
+        Console.Writeline(myName);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
