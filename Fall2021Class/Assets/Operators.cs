3faff81b9dcea60c87336a524341a65684021bdb
Caleb Terrasas <caleb.terrasas@uvu.edu>
2021-10-02 00:59:54 -0600

    Created Operators and practiced commits.

---
+++ Fall2021Class/Assets/Operators.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Operators : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.Writeline(10*5);
+        int x = 10;
+        x++;
+        int x =10;  
+        x += 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
