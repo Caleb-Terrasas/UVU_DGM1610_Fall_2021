52e42edc350d4c086fba174137803ff61a560971
Caleb Terrasas <caleb.terrasas@uvu.edu>
2021-10-06 13:07:02 -0600

    More testing with Conditionals

--- Fall2021Class/Assets/Conditionals.cs
+++ Fall2021Class/Assets/Conditionals.cs
@@ -6,8 +6,19 @@ public class Conditionals : MonoBehaviour
 {
     int x = 50;
     int y = 50;
-    if (x = y)
+    if (x == y)
     {
         Console.Writeline("Hello World");
     }
 }
+{int x = 50;
+ int y = 50;
+ if (x == y)
+ {
+     Console.Writeline("Yes");
+ }
+ else
+ {
+     Console.Writeline("No");
+ }
+ }
