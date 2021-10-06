4385c2dd992f6802c3abde80fb04f526f2a10cfd
Caleb Terrasas <caleb.terrasas@uvu.edu>
2021-10-06 13:03:08 -0600

    Created Conditionals Script and tested

---
+++ Fall2021Class/Assets/Conditionals.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    int x = 50;
+    int y = 50;
+    if (x = y)
+    {
+        Console.Writeline("Hello World");
+    }
+}
