eb41453590a508b19b1b802c785f08f641539497
Caleb Terrasas <caleb.terrasas@uvu.edu>
2021-10-27 15:06:41 -0600

    Edited SpawnManager to spawn balls at interval between 3 and 5

--- Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -22,10 +22,10 @@ public class SpawnManagerX : MonoBehaviour
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
-        int ballIndex = Random.Range(0, ballPrefabs.Length);
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
-
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
