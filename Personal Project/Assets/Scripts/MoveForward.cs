e0be70395651d99294b7848db6543d7130cdc1d8
Caleb Terrasas <caleb.terrasas@uvu.edu>
2021-11-12 13:02:23 -0700

    Added function to destroy projectile once it leaves scene

--- Personal Project/Assets/Scripts/MoveForward.cs
+++ Personal Project/Assets/Scripts/MoveForward.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     public float speed = 40.0f;
+    private float rightBound = 100.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,11 @@ public class MoveForward : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.right * Time.deltaTime * speed);
+
+        //Destroy projectile once it leaves scene
+        if(transform.position.x > rightBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
